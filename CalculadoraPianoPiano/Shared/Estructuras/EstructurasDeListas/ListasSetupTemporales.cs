fa088578c427d54fb87e91bb258dae3e068ceecb
abert <robertbrew98@gmail.com>
2024-01-15 12:09:11 -0400

    listo usar cascading values para escoger cosas de setup y mostrar el resumen, falta que al mostrar cosas del resumen pueda eliminar y editar

---
+++ CalculadoraPianoPiano/Shared/Estructuras/EstructurasDeListas/ListasSetupTemporales.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace CalculadoraPianoPiano.Shared.Estructuras.EstructurasDeListas
+{
+	public class ListasSetupTemporales
+	{
+        //Listas para TIC
+        public List<Setup>? ListaTICTelecomunicaciones { get; set; }
+        public List<Setup>? ListaTICBasesDeDatos { get; set; }
+        public List<Setup>? ListaTICServidoresFisicos { get; set; }
+        public List<Setup>? ListaTICServidoresVirtuales { get; set; }
+        public List<Setup>? ListaTICPlataformasCentralizadas { get; set; }
+
+        public ListasSetupTemporales()
+		{
+            //ListaTICTelecomunicaciones = new List<Setup>();
+            //ListaTICBasesDeDatos = new List<Setup>();
+            //ListaTICServidoresFisicos = new List<Setup>();
+            //ListaTICServidoresVirtuales = new List<Setup>();
+            //ListaTICPlataformasCentralizadas = new List<Setup>();
+		}
+	}
+}
+
---
+++ CalculadoraPianoPiano/Shared/Estructuras/VariablesSetupTIC.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace CalculadoraPianoPiano.Shared
+{
+	public class VariablesSetupTIC
+	{
+
+		public VariablesDeServicio? Telecomunicaciones;
+		public VariablesDeServicio? BasesDeDatos;
+		public VariablesDeServicio? ServidoresFisicos;
+		public VariablesDeServicio? ServidoresVirtuales;
+		public VariablesDeServicio? PlataformasCentralizadas;
+		public VariablesDeServicio? Actualizacion;
+		public VariablesDeServicio? Nubes;
+		public VariablesDeServicio? Distribucion;
+		public VariablesDeServicio? Atencion;
+		public VariablesDeServicio? Estabilizacion;
+		public VariablesDeServicio? Contratos;
+
+        public VariablesSetupTIC()
+		{
+		}
+
+		public void setVariablesTelco(string area, List<string> variables)
+		{
+			switch (area)
+			{
+				case "Telecomunicaciones":
+					Telecomunicaciones = new VariablesDeServicio(variables);
+					break;
+				case "Bases de Datos":
+					BasesDeDatos = new VariablesDeServicio(variables);
+					break;
+				case "Servidores Fisicos":
+                    ServidoresFisicos = new VariablesDeServicio(variables);
+                    break;
+				case "Servidores Virtuales":
+                    ServidoresVirtuales = new VariablesDeServicio(variables);
+                    break;
+				case "Plataformas Centralizadas":
+                    PlataformasCentralizadas = new VariablesDeServicio(variables);
+                    break;
+				default:
+					Console.Write("Error");
+					break;
+			}
+		
+		}
+
+	}
+}
+
