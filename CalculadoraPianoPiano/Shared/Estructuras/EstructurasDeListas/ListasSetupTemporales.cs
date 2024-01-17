c8d7f9fe8dd82da1c887d797548d4659e5ea4891
abert <robertbrew98@gmail.com>
2024-01-17 11:03:33 -0400

    listo el calculo del costo por equipo, por sector, y total del setup en TIC. Hay un bug en la seleccion de opciones en el resumen, creo que porque el if esta alterando el orden de la lista. Falta que se modifique el costo cuando altero valores. Todavia no se si se estan alterando los Setup como tal.

--- CalculadoraPianoPiano/Shared/Estructuras/EstructurasDeListas/ListasSetupTemporales.cs
+++ CalculadoraPianoPiano/Shared/Estructuras/EstructurasDeListas/ListasSetupTemporales.cs
@@ -6,20 +6,69 @@ namespace CalculadoraPianoPiano.Shared.Estructuras.EstructurasDeListas
 	public class ListasSetupTemporales
 	{
         //Listas para TIC
-        public List<Setup>? ListaTICTelecomunicaciones { get; set; }
-        public List<Setup>? ListaTICBasesDeDatos { get; set; }
-        public List<Setup>? ListaTICServidoresFisicos { get; set; }
-        public List<Setup>? ListaTICServidoresVirtuales { get; set; }
-        public List<Setup>? ListaTICPlataformasCentralizadas { get; set; }
+        public List<Setup> ListaTICTelecomunicaciones { get; set; }
+        public List<Setup> ListaTICBasesDeDatos { get; set; }
+        public List<Setup> ListaTICServidoresFisicos { get; set; }
+        public List<Setup> ListaTICServidoresVirtuales { get; set; }
+        public List<Setup> ListaTICPlataformasCentralizadas { get; set; }
 
         public ListasSetupTemporales()
 		{
-            //ListaTICTelecomunicaciones = new List<Setup>();
-            //ListaTICBasesDeDatos = new List<Setup>();
-            //ListaTICServidoresFisicos = new List<Setup>();
-            //ListaTICServidoresVirtuales = new List<Setup>();
-            //ListaTICPlataformasCentralizadas = new List<Setup>();
-		}
+            ListaTICTelecomunicaciones = new List<Setup>();
+            ListaTICBasesDeDatos = new List<Setup>();
+            ListaTICServidoresFisicos = new List<Setup>();
+            ListaTICServidoresVirtuales = new List<Setup>();
+            ListaTICPlataformasCentralizadas = new List<Setup>();
+        }
+
+        public void resetListasTIC()
+        {
+            ListaTICTelecomunicaciones = new List<Setup>();
+            ListaTICBasesDeDatos = new List<Setup>();
+            ListaTICServidoresFisicos = new List<Setup>();
+            ListaTICServidoresVirtuales = new List<Setup>();
+            ListaTICPlataformasCentralizadas = new List<Setup>();
+        }
+
+        public double calcularCostoTotal(string sabor)
+        {
+            double costoTotal = 0;
+            switch (sabor) {
+                case "TIC":
+                    foreach (Setup setup in ListaTICBasesDeDatos)
+                    {
+                        costoTotal += setup.Costo;
+                    }
+                    foreach (Setup setup in ListaTICTelecomunicaciones)
+                    {
+                        costoTotal += setup.Costo;
+                    }
+                    foreach (Setup setup in ListaTICServidoresFisicos)
+                    {
+                        costoTotal += setup.Costo;
+                    }
+                    foreach (Setup setup in ListaTICServidoresVirtuales)
+                    {
+                        costoTotal += setup.Costo;
+                    }
+                    foreach (Setup setup in ListaTICPlataformasCentralizadas)
+                    {
+                        costoTotal += setup.Costo;
+                    }
+                    break;
+
+                case "IoT":
+                    break;
+                case "PMO":
+                    break;
+                case "Ciber Seguridad":
+                    break;
+                default:
+                    break;
+            }
+            return costoTotal;
+
+        }
 	}
 }
 
